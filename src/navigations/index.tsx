84339af19e90d01493ef65a3540f0584cc8470b8
Renato Santos <renato_jfs@hotmail.com>
2021-03-10 01:33:24 -0300

    navigations e assets

---
+++ src/navigations/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const navigations: React.FC = () => {
+  return <View />;
+};
+
+export default navigations;
